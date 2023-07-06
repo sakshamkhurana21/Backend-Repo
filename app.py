8b6898d9fcf49f666cb06522872a4f19864ff442
Saksham <u7502798@anu.edu.au>
2023-07-06 16:21:08 +1000

    Changes Updated

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain.document_loaders import TextLoader
 import os
 from langchain.embeddings import HuggingFaceEmbeddings
 
-directory = "doc/"
+directory = "doc"
 
 
 app = Flask(__name__)
@@ -23,17 +23,20 @@ def extract_information():
     emb = get_similar_docs(query)
     summary= summarize_text(emb)
     final = summary_to_chatgpt(summary, query)
-    return {'ChatGPt Respone': final }
+    return {'ChatGPt Response': final }
 
 
             
 def get_similar_docs(query):
+    print(31)
     if query:
         docs = []
+        print(os.listdir())
         for file in os.listdir(directory):
-            if file != ".ipynb_checkpoints":
+            if file[0] != ".":
                 loader = TextLoader(directory + '/' + file)
-        docs.extend(loader.load())
+                print("loader: ",loader)
+                docs.extend(loader.load())
         embeddings = HuggingFaceEmbeddings()
         db = FAISS.from_documents(docs, embeddings)
         docs = db.similarity_search(query)
@@ -43,21 +46,23 @@ def get_similar_docs(query):
 
 
 def summarize_text(text_list):
-    text = " ".join(text_list)
+    array=[]
+    for items in text_list:
+        array.append(items.page_content)
+    text = "-----END OF DOCUMENT-----".join(array)
     endpoint = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
         "Authorization": ""
     }
     data = {
-        "messages" : [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": f'hi!'}],
+        "messages" : [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": f'Summarise the following documents. Ensure you summarise one document at a time and retain the structure. The documents to summarise are: {text}'}],
         "temperature" : 0.3,
         "max_tokens": 100,
         "model": "gpt-3.5-turbo"
     }
     response = requests.post(endpoint, json=data, headers=headers)
     response.raise_for_status()
-    # print(response.json()["choices"][0]["message"]["content"])
     return response.json()["choices"][0]["message"]["content"].strip()
 
 
@@ -75,7 +80,6 @@ def summary_to_chatgpt(text, query):
     }
     response = requests.post(api_url, json=data, headers=headers)
     response.raise_for_status()
-    # print(response)
     return response.json()["choices"][0]["message"]["content"].strip()
 
 
